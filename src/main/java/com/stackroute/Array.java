f0d04a5f5e4407a2075d9e93da90018217724cc1
Manish <manish.perumalla@cgi.com>
2019-08-03 11:30:05 +0530

    MultiDimensionalArray Method Added

--- src/main/java/com/stackroute/Array.java
+++ src/main/java/com/stackroute/Array.java
@@ -3,10 +3,10 @@ package com.stackroute;
 public class Array {
     public static void main(String args[]) {
     Array array = new Array();
-    array.Arraymethod();
+    array.singleDimensionArraymethod();
     }
 
-    public void Arraymethod() {
+    public void singleDimensionArraymethod() {
         int a[] = new int[5];//declaration and instantiation
         a[0] = 10;//initialization
         a[1] = 20;
@@ -16,6 +16,17 @@ public class Array {
 //traversing array
         for (int i = 0; i < a.length; i++) {//length is the property of array
             System.out.println(a[i]);
+        }
+    }
+    public void twoDimensionalArray(){
+        //declaring and initializing 2D array
+        int arr[][]={{1,2,3},{2,4,5},{4,4,5}};
+//printing 2D array
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
         }
     }
 }
